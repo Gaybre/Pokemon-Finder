e3140e72f9c9e3de05678801cefa58261ef63cf7
JuanGF <jugaybre@gmail.com>
2020-09-23 18:30:54 -0500

    Updates

---
+++ precache-manifest.960035b88c75b521670c27cdb10b1189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f80b474396c1b213338fd6e3a394f93",
+    "url": "/Pokemon-Finder/index.html"
+  },
+  {
+    "revision": "496c1b38c47ea637b2d9",
+    "url": "/Pokemon-Finder/static/js/2.04c38c35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokemon-Finder/static/js/2.04c38c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5fbf28d9cdb7387034",
+    "url": "/Pokemon-Finder/static/js/main.648d3da7.chunk.js"
+  },
+  {
+    "revision": "5e0a538aadbc49d9646a",
+    "url": "/Pokemon-Finder/static/js/runtime-main.d7969089.js"
+  },
+  {
+    "revision": "f829bed61f75627eea111dfde089fe2c",
+    "url": "/Pokemon-Finder/static/media/capturado.f829bed6.png"
+  }
+]);
